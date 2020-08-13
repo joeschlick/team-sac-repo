38008922ee49e0b7ad762d55e8df926ca0a24031
slowdive <schlickjoe@gmail.com>
2020-08-13 00:27:18 -0700

    index - added id's to input fields. Added code for an embeded corona virus map (lines 62-64) but could not make it work yet. Script - added click function for search inputs, AJAX for NYT API, and function to search API with input data. it works

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,31 @@
 //api for covid data by state http://coronavirusapi.com/getTimeSeries/[2 letter state abbreviation]
 //openweathermap current conditions url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchField + "&appid=53d2f99f562f36701d4bf49111eb24c6",
+$(document).ready(function () {
+    var runSearch = $("#run-search");
+    
+    
+    runSearch.click(function () {
+        var searchCity = $("#search-city").val()
+        var searchState = $("#search-state").val()
+        console.log("click")
+        console.log(searchCity)
+        console.log(searchState)
+        articleSearch(searchCity, searchState) 
+    })
 
-console.log("loaded the javascript")
+    function articleSearch(city, state) {
+        var newsQueryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + city + " " + state + "&api-key=6bx62GJuVcNmyKs61ltfKzawldq2gfs0"
+        console.log(newsQueryURL)
+        $.ajax({
+            url: newsQueryURL,
+            method: "GET",
+            dataType: "json"
+        }).then(function(response) {
+            console.log(response)
+        })
+    }
+    
+
+})
+
+
