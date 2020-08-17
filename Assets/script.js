9a5f49723e0e14628db37345a4650950df202511
slowdive <schlickjoe@gmail.com>
2020-08-16 17:55:14 -0700

    added header html and css. Deleted duplicate CSS

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,3 @@
-//api for covid data by state http://coronavirusapi.com/getTimeSeries/[2 letter state abbreviation]
-//openweathermap current conditions url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchField + "&appid=53d2f99f562f36701d4bf49111eb24c6",
 $(document).ready(function () {
 
   var runSearch = $("#run-search");
@@ -94,12 +92,12 @@ function Forecast() {
     articleSearch(searchCity, searchState);
   });
 
-  function articleSearch(city, state) {
+  function articleSearch(city1, state1) {
     var newsQueryURL =
       "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
-      city +
+      city1 +
       " " +
-      state +
+      state1 +
       "&api-key=6bx62GJuVcNmyKs61ltfKzawldq2gfs0";
     console.log(newsQueryURL);
     $.ajax({
